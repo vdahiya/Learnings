b6a33c95276fefdc46d287eebd68e98cddddfb31
Vaibhav Dahiya <vaibhavdahiya@Vaibhavs-Air.bbrouter>
2022-12-28 15:18:10 +0530

    add
    Signed-off-by: Vaibhav Dahiya <vaibhavdahiya@Vaibhavs-Air.bbrouter>

---
+++ scott_meyers/ch2/copy_ctor_with_derived_class.cpp
@@ -0,0 +1,90 @@
+If the copy constructor for the base class is not called when creating a copy of a derived class object, the base class portion of the derived class object will not be properly initialized. This can lead to undefined behavior, such as data corruption or other issues.
+
+For example, consider the following code:
+
+#include <iostream>
+
+class Base {
+public:
+Base(int initialValue = 0): x(initialValue) {}
+Base(const Base& rhs): x(rhs.x) {}
+int getX() const { return x; }
+
+private:
+int x;
+};
+
+class Derived: public Base {
+public:
+Derived(int initialValue)
+: Base (initialValue), y(initialValue) {}
+Derived(const Derived& rhs) : y(rhs.y) {} // copy constructor omits base class initialization
+int getY() const { return y; }
+
+private:
+int y;
+};
+
+int main()
+{
+Derived d1(10);
+Derived d2(d1); // copy d1 into d2
+std::cout << d2.getX() << std::endl; // undefined behavior: x was not initialized in d2
+std::cout << d2.getY() << std::endl; // output: 10
+
+Copy code
+return 0;
+}
+
+In this code, the Derived class has a copy constructor that omits the call to the base class copy constructor. When creating a copy of a Derived object, this can lead to undefined behavior, because the base class portion of the object is not properly initialized. In this case, calling d2.getX() could potentially result in any value being printed, or it could cause a runtime error.
+
+To avoid such issues, it is important to always call the copy constructor for the base class when defining a copy constructor for a derived class. This ensures that the base class portion of the derived class object is properly initialized.
+
+
+
+
+Vaibhav Dahiya
+Can u put all this with main function and explanation as well in comments ?
+Certainly! Here is an example of how omitting the call to the base class copy constructor in a derived class copy constructor can lead to undefined behavior:
+
+#include <iostream>
+
+// Base class with a copy constructor
+class Base {
+public:
+Base(int initialValue = 0): x(initialValue) {}
+Base(const Base& rhs): x(rhs.x) {} // copy constructor initializes x
+int getX() const { return x; }
+
+private:
+int x;
+};
+
+// Derived class with a copy constructor that omits the call to the base class copy constructor
+class Derived: public Base {
+public:
+Derived(int initialValue)
+: Base (initialValue), y(initialValue) {}
+Derived(const Derived& rhs) : y(rhs.y) {} // copy constructor omits base class initialization
+int getY() const { return y; }
+
+private:
+int y;
+};
+
+int main()
+{
+Derived d1(10); // create a derived object with initial value 10
+Derived d2(d1); // copy d1 into d2
+
+Copy code
+// Print the values of x and y in d2
+std::cout << "d2.x: " << d2.getX() << std::endl; // undefined behavior: x was not initialized in d2
+std::cout << "d2.y: " << d2.getY() << std::endl; // output: 10
+
+return 0;
+}
+
+In this code, the Derived class has a copy constructor that omits the call to the base class copy constructor. When creating a copy of a Derived object, this can lead to undefined behavior, because the base class portion of the object is not properly initialized. In this case, calling d2.getX() could potentially result in any value being printed, or it could cause a runtime error.
+
+To avoid such issues, it is important to always call the copy constructor for the base class when defining a copy constructor for a derived class. This ensures that the base class portion of the derived class object is properly initialized.
